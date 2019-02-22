d287d823830d34c45af53666b644bbc61ea4f786
Alex <alexcannon2018@gmail.com>
2019-02-22 13:15:33 -0700

    feature: added certifications, projects, and portfolio pic to repo

--- challenges/javascript/understanding-undefined-value-returned-from-a-function.md
+++ challenges/javascript/understanding-undefined-value-returned-from-a-function.md
@@ -1,9 +1,14 @@
-### Understanding undefined value returned from a function
+---
+title: "Algorithm: Understanding Undefined Value Returned from a Function"
+certificate: "Javascript Algorithms and Data Structures"
+order: 0
+---
+Certificate: *Javascript Algorithms and Data Structures*
 
 #### { Instructions }
 Create a function addFive without any arguments. This function adds 5 to the sum variable, but its returned value is undefined.
 
-#### { Solution }
+#### { My Solution }
 ```
 var sum = 0;
 
--- public/script.js
+++ public/script.js
@@ -1,17 +1,57 @@
-$(document).ready(function() {
-  var topCard = 
-  '<div class="card">' +
-    '<div class="card-body">';
-  var bottomCard =
-    '</div>' +
-  '</div>';
+// Script.js for site
+var challenges = [];
+var projects = [];
+var certsVisible = true;
 
+$(document).ready(function() {
+
+  // Fetch Challenges
   $.getJSON('/challenges/javascript', (data) => {
-    data.forEach(html => {
-      console.log(html);
+    challenges = data;
+    challenges.forEach((item, i) => {
       $("#challenge-list").append(
-        topCard + html + bottomCard
+        '<div class="card" >' +
+          '<div class="card-header" id="head-' + i + '" onclick="return handleCard(event);">' +
+            (item.data ? item.data.title : 'Challenge') +
+          '</div>' +
+          '<div class="card-body" id="challenge-' + i + '">'+
+            '<div class="card-body-open">' +
+              item.html +
+            '</div>' +
+          '</div>' +
+        '</div>'
       );
-    })
+
+      $("#challenge-" + i).children().hide();
+    });
+  });
+
+  $("#certifications").click((e) => {
+    certsVisible = !certsVisible;
+    
+    if (certsVisible) {
+      $(".show-certifications").children().show();
+    } else {
+      $(".show-certifications").children().hide();
+    }
   });
 });
+
+// Handle Pagination buttons
+
+// Handle Card collapse
+function handleCard(e) {
+  const target = e.srcElement.id[e.srcElement.id.length - 1];
+  challenges[target].visible = !challenges[target].visible;
+
+  if (challenges[target].visible) {
+    $("#challenge-" + target).children().show();
+  } else {
+    $("#challenge-" + target).children().hide();
+  }
+}
+
+function toggleMe(target) {
+  console.log("$("+target+")");
+  $(target).children().show();
+}
